2bbc49e6428a94e46d9da1ccd0267f77138d7b39
stu8king <stu8king@gmail.com>
2023-08-23 08:38:19 -0400

    Update settings.py

--- ProjectAlpha/settings.py
+++ ProjectAlpha/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-7wdzw^mq5o67v*fju@hs$mvd_fn0u1iqzr-_byo4y@nog%$$on
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.198.109.156', '127.0.0.1']
+ALLOWED_HOSTS = ['3.223.201.144', '127.0.0.1']
 
 
 # Application definition
