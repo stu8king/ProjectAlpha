f2da3849e58656adea0bb329ac3ff3c3e20bf071
stu8king <stu8king@gmail.com>
2023-08-28 14:44:19 -0400

    first commit

--- ProjectAlpha/urls.py
+++ ProjectAlpha/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=True), name='root_redirect'),
     path('admin/', admin.site.urls),
     path('OTRisk/', include('OTRisk.urls', namespace='OTRisk')),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
